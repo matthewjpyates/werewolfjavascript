d85c672cc4c4171058b7348e73db2fe92fa9b13e
Ubuntu <ubuntu@ip-172-31-38-12.us-east-2.compute.internal>
2020-04-20 12:24:42 +0000

    te

---
+++ move_to_prod.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+sudo cp script.js /var/www/html/webchat/
+sudo cp index.html  /var/www/html/webchat/
+sudo cp style.css /var/www/html/webchat/
+
+sudo cp manifest.webmanifest /var/www/html/webchat/
+
+index.html  manifest.webmanifest  move_to_prod.sh  ntru.js  script.js  style.css
