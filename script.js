490f2e38b5ca3af7ffbfa5cbffa1a69f0b6436f4
Matthew Yates <matt.j.p.yates@gmail.com>
2020-06-06 23:45:33 -0400

    removing the stop

--- script.js
+++ script.js
@@ -122,8 +122,8 @@ function parseKeyFileContent(file) {
       local_key_pair.privateKey = convert_to_javascript_format_from_java(parts[0]);
       local_key_pair.publicKey = convert_to_javascript_format_from_java(parts[1]);
       change_chat_id(parts[2]);
-      stop_pulling_messages();
-      get_token(start_pulling_messages());
+      //stop_pulling_messages();
+      get_token(/*start_pulling_messages()*/);
 
     }
     else {
